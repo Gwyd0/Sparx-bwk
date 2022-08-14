cc9e9b3bd87f03582932b1c1f2cf08d6e4d868f7
Gwydion <68437867+Gwyd0@users.noreply.github.com>
2022-08-14 16:27:16 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -4,153 +4,118 @@ from datetime import datetime
 import time
 import json
 
-File_Name = datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
-codes = []
-
+FILE_NAME = datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
+CODES = []
 
 class info:
-    un = ""
-    pw = ""
-    PATH = 'chromedriver.exe'
-    v = '1.2'
+    USERNAME = ""
+    PASSWORD = ""
+    PATH = 'chromedriver.exe' # path to chromedriver
+    VERSION = '1.2'
 
     lastmsg = ""
-
-    loggedin = True
     isopen = True
-
     autocontinue = False
     autobwk = False
-    isaved = False
 
 
-def log(l):
-    if info.lastmsg == l:
+def log(message):
+    if info.lastmsg == message:
         return
     t = time.localtime()
     current_time = time.strftime("%H:%M:%S", t)
     try:
-        f = open("logs/Log_{0}.txt".format(File_Name), "a")
-        f.write(str(current_time) + " " + l + "\n")
+        f = open("logs/Log_{0}.txt".format(FILE_NAME), "a")
+        f.write("{0} {1} \n".format(str(current_time),message))
         f.close()
     except:
         log("[ERROR] 3 - Failed to log")
 
-    print(current_time + " " + l)
-    info.lastmsg = l
-    codes.append(current_time + " " + l)
+    print(current_time + " " + message)
+    info.lastmsg = message
 
 
 def savesettings():
-    data = {'settings': [{'un': info.un, 'pw': info.pw, 'acon': info.autocontinue, 'abwk': info.autobwk}]}
-    with open('Logs/settings.json', 'w') as outfile:
-        json.dump(data, outfile)
+    info.USERNAME = input("[SETTINGS] Please enter your sparx username: ")
+    info.PASSWORD = input("[SETTINGS] Please enter your sparx password: ")
+    text1 = input("[SETTINGS] Do You want Autocontinue On? (skips games): ").casefold()
+    text2 = input("[SETTINGS] Do You want Autobwk On? (Does Bookwork CODES may fail): ").casefold()
+    text3 = input("[SETTINGS] Save Settings?: ").casefold()
+
+    if text1 == "true" or text1 == "yes":
+        info.autocontinue = True
+        print("[SETTINGS] autocontinue enabled")
+    elif text2 == "true" or text2 == "yes":
+        info.autobwk = True
+        print("[SETTINGS] autobwk enabled")
+    elif text3 == "true" or text3 == "yes":
+        print("[SETTINGS] saving settings. This may take some time")
+        
+        data = {'settings': [{'USERNAME': info.USERNAME, 'PASSWORD': info.PASSWORD, 'acon': info.autocontinue, 'abwk': info.autobwk}]}
+        with open('Logs/settings.json', 'w') as outfile:
+            json.dump(data, outfile)
+        return
+    else:
+        print("[Error] Invalid argument")
+        exit
 
 
 def loadsettings():
     try:
         with open('Logs/settings.json') as json_file:
             data = json.load(json_file)
-            info.isaved = True
-
-            info.un = data['settings'][0]['un']
-            info.pw = data['settings'][0]['pw']
+            print("[SETTINGS] Loading settings. This may take some time")
+            info.USERNAME = data['settings'][0]['USERNAME']
+            info.PASSWORD = data['settings'][0]['PASSWORD']
             info.autocontinue = data['settings'][0]['acon']
             info.autobwk = data['settings'][0]['abwk']
+            return True
 
     except:
-        info.isaved = False
+        return False
 
 
 def makelogfile():
     try:
-        f = open("logs/Log_{0}.txt".format(File_Name), "a")
-        f.write("[SPARX BWK LOGS]\n")
-        f.write("[START TIME] " + File_Name + "\n")
-        f.write("[Logged In] " + str(info.loggedin) + "\n")
-        f.write("--- [SETTINGS] --- \n")
-        f.write("[USER] " + info.un + "\n")
-        f.write("[PASSWORD] " + info.pw + "\n")
-        f.write("[AUTOCONTINUE] " + str(info.autocontinue) + "\n")
-        f.write("[AUTOBWC] " + str(info.autobwk) + "\n")
-        f.write("[IS-SAVED] " + str(info.isaved) + "\n")
-        f.write("[VERSION] " + info.v + "\n")
-        f.write("--- [WORK LOGS] --- \n")
+        f = open("logs/Log_{0}.txt".format(FILE_NAME), "a")
+        f.write("[SPARX BWK LOGS]\n[START TIME] {0}\n[Logged In] {1} \n--- [SETTINGS] --- \n[USER] {2}\n[PASSWORD] {3}\n[AUTOCONTINUE] {4}\n[AUTOBWC] {5}\n[IS-SAVED] {6}\n[VERSION] {7}\n--- [WORK LOGS] --- \n" .format(
+            FILE_NAME,str(info.loggedin),info.USERNAME,info.PASSWORD,str(info.autocontinue),str(info.autobwk),str(info.isaved),info.VERSION))
         f.close()
         log("[LOG] Writing New Log")
-        log("[LOG] New Log Made [NAME] " + File_Name)
+        log("[LOG] New Log Made [NAME] " + FILE_NAME)
+        return
     except:
         log("[ERROR] 2 - FAILED TO WRITE LOG")
-        exit()
+        return
 
 
 def start():
-    print(
-        " ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄       ▄       ▄▄▄▄▄▄▄▄▄▄   ▄         ▄  ▄    ▄  \n"
-        "▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌     ▐░▌     ▐░░░░░░░░░░▌ ▐░▌       ▐░▌▐░▌  ▐░▌ \n"
-        "▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌ ▐░▌   ▐░▌      ▐░█▀▀▀▀▀▀▀█░▌▐░▌       ▐░▌▐░▌ ▐░▌  \n"
-        "▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌  ▐░▌ ▐░▌       ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌▐░▌   \n"
-        "▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌   ▐░▐░▌        ▐░█▄▄▄▄▄▄▄█░▌▐░▌   ▄   ▐░▌▐░▌░▌    \n"
-        "▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌    ▐░▌         ▐░░░░░░░░░░▌ ▐░▌  ▐░▌  ▐░▌▐░░▌     \n"
-        " ▀▀▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀█░█▀▀    ▐░▌░▌        ▐░█▀▀▀▀▀▀▀█░▌▐░▌ ▐░▌░▌ ▐░▌▐░▌░▌    \n"
-        "          ▐░▌▐░▌          ▐░▌       ▐░▌▐░▌     ▐░▌    ▐░▌ ▐░▌       ▐░▌       ▐░▌▐░▌▐░▌ ▐░▌▐░▌▐░▌▐░▌   \n"
-        " ▄▄▄▄▄▄▄▄▄█░▌▐░▌          ▐░▌       ▐░▌▐░▌      ▐░▌  ▐░▌   ▐░▌      ▐░█▄▄▄▄▄▄▄█░▌▐░▌░▌   ▐░▐░▌▐░▌ ▐░▌  \n"
-        "▐░░░░░░░░░░░▌▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌     ▐░▌     ▐░░░░░░░░░░▌ ▐░░▌     ▐░░▌▐░▌  ▐░▌ \n"
-        " ▀▀▀▀▀▀▀▀▀▀▀  ▀            ▀         ▀  ▀         ▀  ▀       ▀       ▀▀▀▀▀▀▀▀▀▀   ▀▀       ▀▀  ▀    ▀  \n"
-    )
-    print("By 7xzb  v." + info.v)
-    loadsettings()
+    print(" ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄       ▄       ▄▄▄▄▄▄▄▄▄▄   ▄         ▄  ▄    ▄  \n""▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌     ▐░▌     ▐░░░░░░░░░░▌ ▐░▌       ▐░▌▐░▌  ▐░▌ \n""▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌ ▐░▌   ▐░▌      ▐░█▀▀▀▀▀▀▀█░▌▐░▌       ▐░▌▐░▌ ▐░▌  \n""▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌  ▐░▌ ▐░▌       ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌▐░▌   \n""▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌   ▐░▐░▌        ▐░█▄▄▄▄▄▄▄█░▌▐░▌   ▄   ▐░▌▐░▌░▌    \n""▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌    ▐░▌         ▐░░░░░░░░░░▌ ▐░▌  ▐░▌  ▐░▌▐░░▌     \n"" ▀▀▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀█░█▀▀    ▐░▌░▌        ▐░█▀▀▀▀▀▀▀█░▌▐░▌ ▐░▌░▌ ▐░▌▐░▌░▌    \n""          ▐░▌▐░▌          ▐░▌       ▐░▌▐░▌     ▐░▌    ▐░▌ ▐░▌       ▐░▌       ▐░▌▐░▌▐░▌ ▐░▌▐░▌▐░▌▐░▌   \n"" ▄▄▄▄▄▄▄▄▄█░▌▐░▌          ▐░▌       ▐░▌▐░▌      ▐░▌  ▐░▌   ▐░▌      ▐░█▄▄▄▄▄▄▄█░▌▐░▌░▌   ▐░▐░▌▐░▌ ▐░▌  \n""▐░░░░░░░░░░░▌▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌     ▐░▌     ▐░░░░░░░░░░▌ ▐░░▌     ▐░░▌▐░▌  ▐░▌ \n"" ▀▀▀▀▀▀▀▀▀▀▀  ▀            ▀         ▀  ▀         ▀  ▀       ▀       ▀▀▀▀▀▀▀▀▀▀   ▀▀       ▀▀  ▀    ▀  \nBy Gwyd0  VERSION." + info.VERSION)
+    
+    if not loadsettings(): # if loadsettings returns false then savesettings for next time.
+        savesettings()
 
-    if not info.isaved:
-        info.un = input("[SETTINGS] Please enter your sparx username: ")
-        info.pw = input("[SETTINGS] Please enter your sparx password: ")
-        text1 = input("[SETTINGS] Do You want Autocontinue On? (skips games): ").casefold()
-        text2 = input("[SETTINGS] Do You want Autobwk On? (Does Bookwork codes may fail): ").casefold()
-        text3 = input("[SETTINGS] Save Settings?: ").casefold()
-
-        makelogfile()
-
-        if text1 == "true" or text1 == "yes":
-            info.autocontinue = True
-            log("[SETTINGS] autocontinue enabled")
-        if text2 == "true" or text2 == "yes":
-            info.autobwk = True
-            log("[SETTINGS] autobwk enabled")
-        if text3 == "true" or text3 == "yes":
-            log("[SETTINGS] saving settings. This may take some time")
-            savesettings()
-    else:
-        makelogfile()
-        log("[SETTINGS] Loading settings. This may take some time")
+    makelogfile()
 
     driver = webdriver.Chrome(info.PATH)
     driver.get("https://auth.sparxmaths.uk/oauth2/auth?client_id=sparx-maths-sw")
 
-    log("[MAIN] If chrome fails to open. install the newest version of chromedriver.")
+    USERNAME_ELEMENT = driver.find_element_by_id("username")
+    PASSWORD_ELEMENT = driver.find_element_by_id("password")
 
-    un_ui = driver.find_element_by_id("username")
-    pd_ui = driver.find_element_by_id("password")
-
-    un_ui.send_keys(info.un)
-    pd_ui.send_keys(info.pw)
-    pd_ui.send_keys(Keys.RETURN)
+    USERNAME_ELEMENT.send_keys(info.USERNAME)
+    PASSWORD_ELEMENT.send_keys(info.PASSWORD)
+    PASSWORD_ELEMENT.send_keys(Keys.RETURN)
 
     log("[MAIN] Chrome Version: " + str(driver.capabilities['browserVersion']))
+    log("[MAIN] If chrome fails to open. install the newest version of chromedriver.")
     try:
         mainloop(driver)
     except:
+        log("[MAIN] Chrome Closed. Exiting.")
         info.isopen = False
-        log("[MAIN] Chrome Closed")
+        exit()
 
-        textrun = input("[MAIN] RESTART?: ").casefold()
-
-        if textrun == "true" or textrun == "yes":
-            log("[MAIN] RESTARTING CHROME")
-            info.isopen = True
-            start()
-        else:
-            driver.quit()
-            exit()
 
 
 def mainloop(driver):
@@ -162,6 +127,7 @@ def mainloop(driver):
                     kp = driver.find_element_by_class_name('number-input')
                     if kp.get_attribute("value") != "":
                         log("[BWK] " + BWK.text + " [ANSWER] " + kp.get_attribute("value"))
+                        #CODES.append(BWK.text + "/" + kp.get_attribute("value")) for auto checks
                 except:
                     try:
                         sl = driver.find_element_by_class_name('selected')
